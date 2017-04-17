0b21068e553c80e6c21e57b83d5a0523216785d7
Nagarjun Govindraj <nagarjun.govindraj@imaginea.com>
2017-04-17 18:04:53 +0530

    Modified detect hijacks to poll for directory changes using watchdog library

--- examples/annotation/detect_hijacks.py
+++ examples/annotation/detect_hijacks.py
@@ -1,16 +1,16 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2016 ANSSI
-# This file is part of the tabi project licensed under the MIT license.
-
 from __future__ import print_function
 
+import sys
+import time
+import logging
+import os
 import json
 import logging
 
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from tabi.emulator import parse_registry_data
 from tabi.emulator import detect_hijacks
-
-logger = logging.getLogger(__name__)
-
 
 def choose_input(input):
     if input == "mabo":
@@ -25,58 +25,94 @@ def choose_input(input):
         raise ValueError("unknown input type {}".format(input))
 
 
-if __name__ == "__main__":
-    import argparse
+def registry_kwargs(file_path):
 
+    if os.path.basename(file_path) == "routes.csv":
+        kwargs["irr_ro_file"] = os.path.basename(file_path)
+
+    if os.path.basename(file_path) == "roa.csv":
+        kwargs["rpki_roa_file"] = os.path.basename(file_path)
+
+    if os.path.basename(file_path) == "maintainers.csv":
+        kwargs["irr_mnt_file"] = os.path.basename(file_path)        
+
+    if os.path.basename(file_path) == "organisations.csv":
+        kwargs["irr_org_file"] = os.path.basename(file_path)
+    
+    return kwargs
+
+
+class PollingHandler(FileSystemEventHandler):
+
+    def on_created(self, event):
+        
+        super(PollingHandler, self).on_created(event)
+	
+        what = 'directory' if event.is_directory else 'file'
+        logging.info("Created %s: %s", what, event.src_path)    	
+        
+        if args.registry_path == os.path.dirname(event.src_path):
+            reg_kwargs = registry_kwargs(event.src_path)
+
+            if len(reg_kwargs) == 4:
+                print (reg_kwargs)
+                print ("Completed parsing registry data")
+
+        if args.bgp_path == os.path.dirname(event.src_path):
+            print ("bgp event")
+
+	
+if __name__ == "__main__":
+    
+    import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("mrt_files", nargs="*", help="MRT files")
+
     parser.add_argument("-c", "--collector",
                         help="collector name from where the log files are",
                         default="none")
     parser.add_argument("-i", "--input",
                         help="MRT parser, e.g. 'mabo'",
                         default="mabo")
-    parser.add_argument("-o", "--options",
-                        help="extra options passed to the input method")
-    parser.add_argument("--irr-ro-file",
-                        help="CSV file containing IRR route objects")
-    parser.add_argument("--irr-mnt-file",
-                        help="CSV file containing IRR maintainer objects")
-    parser.add_argument("--irr-org-file",
-                        help="CSV file containing IRR organisation objects")
-    parser.add_argument("--rpki-roa-file",
-                        help="CSV file containing ROA")
+    
+    parser.add_argument("--registry-path",
+                        help="enter the path for registry data")
+
+    parser.add_argument("--bgp-path",
+                        help="enter the path for bgp rib and update files")
     parser.add_argument("-v", "--verbose", action="store_true",
                         help="more logging")
+    
 
-    args = parser.parse_args()
-
+    args = parser.parse_args()    
+    
     logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
 
-    input_kwargs = {"files": args.mrt_files}
-    if args.options is not None:
-        try:
-            input_kwargs.update(dict([w.split("=", 2) for w in
-                                      args.options.split(",")]))
-        except:
-            raise ValueError("invalid input options (use format key=value)")
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    
+    kwargs = {}
+    mrt_files = []
+    observers = []
+    
+    targets = []
+    
+    event_handler = PollingHandler()
+    observer = Observer()        
+    targets.append(args.registry_path)
+    targets.append(args.bgp_path)
+    
+    for path in targets:
+        targetPath = str(path)
+        observer.schedule(event_handler, targetPath, recursive=False)
+        observers.append(observer)
+    
+    observer.start()
 
-    input = choose_input(args.input)
-    kwargs = input(args.collector, **input_kwargs)
+    try:
+        while True:            
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
 
-    if args.irr_ro_file is not None:
-        kwargs["irr_ro_file"] = args.irr_ro_file
-
-    if args.rpki_roa_file is not None:
-        kwargs["rpki_roa_file"] = args.rpki_roa_file
-
-    if args.irr_org_file is not None:
-        kwargs["irr_org_file"] = args.irr_org_file
-
-    if args.irr_mnt_file is not None:
-        kwargs["irr_mnt_file"] = args.irr_mnt_file
-
-    # detect the conflicts and print them
-    for conflict in detect_hijacks(**kwargs):
-        if conflict["type"] == "ABNORMAL":
-            print(json.dumps(conflict))
+    observer.join()
